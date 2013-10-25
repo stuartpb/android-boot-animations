51dbf2e17f0ae9324175bcb6c7a63084680ba74f
Stuart P. Bentley <stuart@testtrack4.com>
2013-10-24 21:33:16 -0700

    Update with partially-working code
    Converted text to path to sidestep GM font issue
    ZIP still doesn't work, but image output does, and that's enough to get
    7-Zip working

--- make-mod-zip.js
+++ make-mod-zip.js
@@ -1,8 +1,8 @@
 var gm = require('gm');
-var jszip = require('node-zip');
+var JSZip = require('jszip');
 var fs = require('fs');
 
-var fullSvg = fs.readFileSync('base.svg',{encoding:'utf8'});
+var fullSvg = fs.readFileSync('base.svg', {encoding:'utf8'});
 
 var caps = /^([\s\S]*)\$TEXT_X_POSITION([\s\S]*)\$TEXT_OPACITY([\s\S]*)$/.exec(fullSvg);
 
@@ -11,10 +11,10 @@ var postOpacBuf = new Buffer(caps[3]);
 
 var midBuf = new Buffer(caps[2]);
 
-var postXBuf = Buffer.concat([midBuf,new Buffer('1'),postOpacBuf]);
-var preOpacBuf = Buffer.concat([postOpacBuf,new Buffer('-1309.1608'),midBuf]);
+var postXBuf = Buffer.concat([midBuf, new Buffer('1'), postOpacBuf]);
+var preOpacBuf = Buffer.concat([preXBuf, new Buffer('-1309.1608'), midBuf]);
 
-var bazip = jszip();
+var bazip = new JSZip();
 
 function padded(n) {
   return ('000'+n).slice(-3);
@@ -29,7 +29,7 @@ function addFileToAnimation(path, buffer) {
 function addScrollFrame(i) {
   var offset = 720 + (-2029.1608 / 120) * i;
   var frameBuf = Buffer.concat([preXBuf,
-    new Buffer(offset.toString()),postXBuf])
+    new Buffer(offset.toString()), postXBuf])
   gm(frameBuf,'frame.svg')
     .toBuffer('png',function (err, buffer) {
     if (err) throw new Error(err);
@@ -37,38 +37,37 @@ function addScrollFrame(i) {
       addFileToAnimation('scroll/' + padded(i) + '.png', buffer);
     }
   });
-  //.write('out/scroll/'+ padded(i) + '.png',function(err){if (err) throw new Error(err);});
 }
 
 function addFadeOut(i) {
   var opacity = 1 - 0.1 * i;
-  var frameBuf = Buffer.concat([preXBuf,
-    new Buffer(opacity.toString()), postXBuf]);
+  var frameBuf = Buffer.concat([preOpacBuf,
+    new Buffer(opacity.toString()), postOpacBuf]);
 
   gm(frameBuf,'frame.svg')
     .toBuffer('png',function (err, buffer) {
     if (err) throw new Error(err);
     else {
-      addFileToAnimation('blink/' + padded(i) + '.png',buffer);
-      addFileToAnimation('fade/' + padded(i) + '.png',buffer);
+      addFileToAnimation('blink/' + padded(i) + '.png', buffer);
+      addFileToAnimation('fade/' + padded(i) + '.png', buffer);
     }
   });
 }
 
 function addBlack(i) {
-  var frameBuf = Buffer.concat([preXBuf, new Buffer('0'), postXBuf]);
+  var frameBuf = Buffer.concat([preOpacBuf, new Buffer('0'), postOpacBuf]);
 
   gm(frameBuf,'frame.svg')
     .toBuffer('png',function (err, buffer) {
     if (err) throw new Error(err);
     else {
-      addFileToAnimation('blink/' + padded(i) + '.png',buffer);
+      addFileToAnimation('blink/' + padded(i) + '.png', buffer);
 
       // Yes, I realize this is a ton of extra complexity
       // for the sake of rendering a completely black PNG.
 
       if (i == 10) {
-        addFileToAnimation('blink/' + padded(i) + '.png',buffer);
+        addFileToAnimation('fade/' + padded(i) + '.png', buffer);
       }
     }
   });
@@ -76,8 +75,8 @@ function addBlack(i) {
 
 function addFadeIn(i) {
   var opacity = (1/3) * (i-12);
-  var frameBuf = Buffer.concat([preXBuf,
-    new Buffer(opacity.toString()), postXBuf]);
+  var frameBuf = Buffer.concat([preOpacBuf,
+    new Buffer(opacity.toString()), postOpacBuf]);
 
   gm(frameBuf,'frame.svg')
     .toBuffer('png',function (err, buffer) {
@@ -104,13 +103,13 @@ for (i = 13; i < 16; i++) {
   addFadeIn(i);
 }
 
-
 bazip.file('desc.txt',fs.readFileSync('desc.txt','utf8'));
 
-//COMMENTME
 fs.writeFileSync('bootanimation.zip',bazip.generate({type:'nodebuffer'}));
 
+/* //commented until I figure out flashable ZIPs
 fs.writeFileSync('big-o-boot-mod.zip',
-  jszip().file('system/media/bootanimation.zip',
+  new JSZip().file('system/media/bootanimation.zip',
     bazip.generate({type:'nodebuffer'}))
   .generate({type:'nodebuffer'}));
+*/
