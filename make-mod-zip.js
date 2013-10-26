5b80d6f57a2e3358ea8c4fdc6a02424d529778bd
Stuart P. Bentley <stuart@testtrack4.com>
2013-10-26 07:55:30 +0000

    Fix zipping (queue-async!!)

--- make-mod-zip.js
+++ make-mod-zip.js
@@ -1,6 +1,7 @@
 var gm = require('gm');
 var JSZip = require('jszip');
 var fs = require('fs');
+var queue = require('queue-async');
 
 var fullSvg = fs.readFileSync('base.svg', {encoding:'utf8'});
 
@@ -21,44 +22,48 @@ function padded(n) {
 }
 
 function addFileToAnimation(path, buffer) {
-  //until I can track down the ZIP issue
-  fs.writeFileSync('out/' + path, buffer);
-  bazip.file(path, buffer);
+  if(bazip){
+    bazip.file(path, buffer);
+  } else {
+    fs.writeFileSync('out/' + path, buffer);
+  }
 }
 
-function addScrollFrame(i) {
+function addScrollFrame(i,cb) {
   var offset = 720 + (-2029.1608 / 120) * i;
   var frameBuf = Buffer.concat([preXBuf,
     new Buffer(offset.toString()), postXBuf])
   gm(frameBuf,'frame.svg')
-    .toBuffer('png',function (err, buffer) {
+    .toBuffer('png', function (err, buffer) {
     if (err) throw new Error(err);
     else {
       addFileToAnimation('scroll/' + padded(i) + '.png', buffer);
+      return cb();
     }
   });
 }
 
-function addFadeOut(i) {
+function addFadeOut(i,cb) {
   var opacity = 1 - 0.1 * i;
   var frameBuf = Buffer.concat([preOpacBuf,
     new Buffer(opacity.toString()), postOpacBuf]);
 
   gm(frameBuf,'frame.svg')
-    .toBuffer('png',function (err, buffer) {
+    .toBuffer('png', function (err, buffer) {
     if (err) throw new Error(err);
     else {
       addFileToAnimation('blink/' + padded(i) + '.png', buffer);
       addFileToAnimation('fade/' + padded(i) + '.png', buffer);
+      return cb();
     }
   });
 }
 
-function addBlack(i) {
+function addBlack(i,cb) {
   var frameBuf = Buffer.concat([preOpacBuf, new Buffer('0'), postOpacBuf]);
 
   gm(frameBuf,'frame.svg')
-    .toBuffer('png',function (err, buffer) {
+    .toBuffer('png', function (err, buffer) {
     if (err) throw new Error(err);
     else {
       addFileToAnimation('blink/' + padded(i) + '.png', buffer);
@@ -69,47 +74,56 @@ function addBlack(i) {
       if (i == 10) {
         addFileToAnimation('fade/' + padded(i) + '.png', buffer);
       }
+      return cb();
     }
   });
 }
 
-function addFadeIn(i) {
+function addFadeIn(i,cb) {
   var opacity = (1/3) * (i-12);
   var frameBuf = Buffer.concat([preOpacBuf,
     new Buffer(opacity.toString()), postOpacBuf]);
 
   gm(frameBuf,'frame.svg')
-    .toBuffer('png',function (err, buffer) {
+    .toBuffer('png', function (err, buffer) {
     if (err) throw new Error(err);
     else {
       addFileToAnimation('blink/' + padded(i) + '.png',buffer);
+      return cb();
     }
   });
 }
 
 var i;
 
+var q = queue();
+
 for (i = 0; i < 120; i++) {
-  addScrollFrame(i);
+  q.defer(addScrollFrame,i);
 }
 
 for (i = 0; i < 10; i++) {
-  addFadeOut(i);
+  q.defer(addFadeOut,i);
 }
 for (i = 10; i < 13; i++) {
-  addBlack(i);
+  q.defer(addBlack,i);
 }
 for (i = 13; i < 16; i++) {
-  addFadeIn(i);
+  q.defer(addFadeIn,i);
 }
 
-bazip.file('desc.txt',fs.readFileSync('desc.txt','utf8'));
+addFileToAnimation('desc.txt',fs.readFileSync('desc.txt'));
 
-fs.writeFileSync('bootanimation.zip',bazip.generate({type:'nodebuffer'}));
+q.awaitAll(function(err){
+  if (err) throw new Error(err);
+  else if (bazip) {
+    fs.writeFileSync('bootanimation.zip',bazip.generate({type:'nodebuffer'}));
 
-/* //commented until I figure out flashable ZIPs
-fs.writeFileSync('big-o-boot-mod.zip',
-  new JSZip().file('system/media/bootanimation.zip',
-    bazip.generate({type:'nodebuffer'}))
-  .generate({type:'nodebuffer'}));
-*/
+    /* //commented until I figure out flashable ZIPs
+    fs.writeFileSync('big-o-boot-mod.zip',
+      new JSZip().file('system/media/bootanimation.zip',
+        bazip.generate({type:'nodebuffer'}))
+      .generate({type:'nodebuffer'}));
+    */
+  }
+});
